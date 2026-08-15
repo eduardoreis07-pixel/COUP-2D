cde2d28c5315494feca6c7bccf0d33c949cf5d86
Eduardo Eufrásio <231809117+eduardoreis07-pixel@users.noreply.github.com>
2026-08-15 15:10:33 -0300

    Check-in inicial

---
+++ Assets/Menager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Menager : MonoBehaviour
+{
+    public int hasDuque = 0;
+    public int hasAssassin = 0;
+    public int hasEmbassor = 0;
+    public int hasCapitan = 0;
+    public int hasCondess = 0;
+
+    public int QtdCoins = 0;
+
+    public List<String> Cards = ["Duque", "Assassino", "Embaixador", "Capitão", "Condessa"];
+    public List<String> Action = ["Renda", "Imposto", "Assassinato", "Troca", "Roubo", "Golpe"];
+
+    void Start()
+    {
+        
+
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
